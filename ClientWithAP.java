11d79739629b23c53b9b5109a2dfda1cd119205f
iFission <alexwwj@icloud.com>
2020-04-12 17:43:44 +0800

    feat: verify server's cert with ca

--- ClientWithAP.java
+++ ClientWithAP.java
@@ -14,55 +14,76 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 class PublicKeyReader {
- 
-	public static PublicKey get(String filename)
-	  throws Exception {
-   
-	  byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
-   
-	  X509EncodedKeySpec spec =
-		new X509EncodedKeySpec(keyBytes);
-	  KeyFactory kf = KeyFactory.getInstance("RSA");
-	  return kf.generatePublic(spec);
+
+	public static PublicKey get(String filename) throws Exception {
+
+		byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		return kf.generatePublic(spec);
+	}
+}
+
+class CertificateReader {
+
+	public static X509Certificate get(String filename) throws Exception {
+		InputStream inputStream = new FileInputStream(filename);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		return (X509Certificate) cf.generateCertificate(inputStream);
 	}
 }
 
 public class ClientWithAP {
 
 	public static void main(String[] args) {
-		try{
-			InputStream inputStream = new FileInputStream("keys_certificate/example-19fb0430-7c8f-11ea-ae9d-89114163ae84.crt");
-			CertificateFactory cf = CertificateFactory.getInstance("X.509");
-			X509Certificate CAcert = (X509Certificate)cf.generateCertificate(inputStream);			
-			CAcert.checkValidity();
-	
-			// get public key
-			PublicKey serverPublicKey = CAcert.getPublicKey();
-			CAcert.verify(serverPublicKey);
+		try {
+			// get server cert
+			X509Certificate serverCert = CertificateReader
+					.get("keys_certificate_2/example-e0067250-7c9d-11ea-ae9d-89114163ae84.crt");
+			serverCert.checkValidity();
+
+			// get server public key
+			PublicKey serverPublicKey = serverCert.getPublicKey();
 			System.out.println(serverPublicKey);
-		}
-		catch (Exception e){
+
+			// get CA cert
+			X509Certificate CAcert = CertificateReader
+					.get("keys_certificate/cacse.crt");
+
+			// get CA public key
+			PublicKey CAPublicKey = CAcert.getPublicKey();
+			System.out.println(CAPublicKey);
+
+			System.out.println("serverPublicKey: " + serverPublicKey);
+			System.out.println("CAPublicKey: " + CAPublicKey);
+			serverCert.verify(CAPublicKey);
+			System.out.println("Server's certificate is verified");
+		} catch (Exception e) {
 			System.out.println("[ERROR!] " + e);
 		}
 
-	String filename = "100.txt";
-    	if (args.length > 0) filename = args[0];
+		String filename = "100.txt";
+		if (args.length > 0)
+			filename = args[0];
 
-    	String serverAddress = "localhost";
-    	if (args.length > 1) filename = args[1];
+		String serverAddress = "localhost";
+		if (args.length > 1)
+			filename = args[1];
 
-    	int port = 4321;
-    	if (args.length > 2) port = Integer.parseInt(args[2]);
+		int port = 4321;
+		if (args.length > 2)
+			port = Integer.parseInt(args[2]);
 
 		int numBytes = 0;
 
 		Socket clientSocket = null;
 
-        DataOutputStream toServer = null;
-        DataInputStream fromServer = null;
+		DataOutputStream toServer = null;
+		DataInputStream fromServer = null;
 
-    	FileInputStream fileInputStream = null;
-        BufferedInputStream bufferedFileInputStream = null;
+		FileInputStream fileInputStream = null;
+		BufferedInputStream bufferedFileInputStream = null;
 
 		long timeStarted = System.nanoTime();
 
@@ -87,10 +108,10 @@ public class ClientWithAP {
 			fileInputStream = new FileInputStream(filename);
 			bufferedFileInputStream = new BufferedInputStream(fileInputStream);
 
-	        byte [] fromFileBuffer = new byte[117];
+			byte[] fromFileBuffer = new byte[117];
 
-	        // Send the file
-	        for (boolean fileEnded = false; !fileEnded;) {
+			// Send the file
+			for (boolean fileEnded = false; !fileEnded;) {
 				numBytes = bufferedFileInputStream.read(fromFileBuffer);
 				fileEnded = numBytes < 117;
 
@@ -100,14 +121,16 @@ public class ClientWithAP {
 				toServer.flush();
 			}
 
-	        bufferedFileInputStream.close();
-	        fileInputStream.close();
+			bufferedFileInputStream.close();
+			fileInputStream.close();
 
 			System.out.println("Closing connection...");
 
-		} catch (Exception e) {e.printStackTrace();}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		long timeTaken = System.nanoTime() - timeStarted;
-		System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run");
+		System.out.println("Program took: " + timeTaken / 1000000.0 + "ms to run");
 	}
 }
