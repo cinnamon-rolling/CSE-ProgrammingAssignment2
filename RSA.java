da9bca9c0e54e27be4e29281646dd31679f8c0cd
iFission <alexwwj@icloud.com>
2020-04-19 16:17:06 +0800

    refactor: rename input variables

--- RSA.java
+++ RSA.java
@@ -3,30 +3,32 @@ import java.security.*;
 import java.security.PublicKey;
 import javax.crypto.Cipher;
 
-public class RSA{
+public class RSA {
 
         // RSA encrypt
-        public static byte[] encrypt(byte[] byteArray, PublicKey serverPublicKey) throws Exception{
+        public static byte[] encrypt(byte[] byteArray, Key key) throws Exception {
                 // instantiate cypher
                 Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-                rsaCipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+                rsaCipher.init(Cipher.ENCRYPT_MODE, key);
 
                 // decrypt message
                 byte[] encryptedBytesArray = rsaCipher.doFinal(byteArray);
-                System.out.println("encryptedBytesArray: " + encryptedBytesArray + "\nLength of encryptedBytesArray: " + encryptedBytesArray.length);
-        
+                System.out.println("encryptedBytesArray: " + encryptedBytesArray + "\nLength of encryptedBytesArray: "
+                                + encryptedBytesArray.length);
+
                 return encryptedBytesArray;
-        } 
+        }
 
         // RSA decrypt
-        public static byte[] decrypt(PrivateKey serverPrivateKey, byte[] byteArray, InputStream inStream) throws Exception{
+        public static byte[] decrypt(byte[] byteArray, Key key) throws Exception {
                 // instantiate cypher
                 Cipher desCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-                desCipher.init(Cipher.DECRYPT_MODE, serverPrivateKey);
-                
+                desCipher.init(Cipher.DECRYPT_MODE, key);
+
                 // decrypt message
                 byte[] decryptedBytesArray = desCipher.doFinal(byteArray);
-                System.out.println("decryptedBytesArray: " + decryptedBytesArray + "\nLength of decryptedBytesArray: " + decryptedBytesArray.length);
+                System.out.println("decryptedBytesArray: " + decryptedBytesArray + "\nLength of decryptedBytesArray: "
+                                + decryptedBytesArray.length);
 
                 return decryptedBytesArray;
         }
