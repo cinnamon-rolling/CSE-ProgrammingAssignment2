6313ecfc5feb2182be6fdbc451ff2ecfbc15163b
iFission <alexwwj@icloud.com>
2020-04-19 17:45:17 +0800

    chore: add debug messages to see packet sent in string

--- ServerWithoutSecurity.java
+++ ServerWithoutSecurity.java
@@ -4,13 +4,15 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Base64;
 
 public class ServerWithoutSecurity {
 
 	public static void main(String[] args) {
 
-    	int port = 4321;
-    	if (args.length > 0) port = Integer.parseInt(args[0]);
+		int port = 4321;
+		if (args.length > 0)
+			port = Integer.parseInt(args[0]);
 
 		ServerSocket welcomeSocket = null;
 		Socket connectionSocket = null;
@@ -26,6 +28,8 @@ public class ServerWithoutSecurity {
 			fromClient = new DataInputStream(connectionSocket.getInputStream());
 			toClient = new DataOutputStream(connectionSocket.getOutputStream());
 
+			int packetCount = 0;
+
 			while (!connectionSocket.isClosed()) {
 
 				int packetType = fromClient.readInt();
@@ -36,20 +40,25 @@ public class ServerWithoutSecurity {
 					System.out.println("Receiving file...");
 
 					int numBytes = fromClient.readInt();
-					byte [] filename = new byte[numBytes];
+					byte[] filename = new byte[numBytes];
 					// Must use read fully!
 					// See: https://stackoverflow.com/questions/25897627/datainputstream-read-vs-datainputstream-readfully
 					fromClient.readFully(filename, 0, numBytes);
 
-					fileOutputStream = new FileOutputStream("recv_"+new String(filename, 0, numBytes));
+					fileOutputStream = new FileOutputStream("recv_" + new String(filename, 0, numBytes));
 					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
 
-				// If the packet is for transferring a chunk of the file
+					// If the packet is for transferring a chunk of the file
 				} else if (packetType == 1) {
 
 					int numBytes = fromClient.readInt();
-					byte [] block = new byte[numBytes];
+					byte[] block = new byte[numBytes];
 					fromClient.readFully(block, 0, numBytes);
+
+					// count and print the packet in string
+					packetCount++;
+					System.out.println("packetCount:" + packetCount);
+					System.out.println(Base64.getEncoder().encodeToString(block));
 
 					if (numBytes > 0)
 						bufferedFileOutputStream.write(block, 0, numBytes);
@@ -57,8 +66,10 @@ public class ServerWithoutSecurity {
 					if (numBytes < 117) {
 						System.out.println("Closing connection...");
 
-						if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
-						if (bufferedFileOutputStream != null) fileOutputStream.close();
+						if (bufferedFileOutputStream != null)
+							bufferedFileOutputStream.close();
+						if (bufferedFileOutputStream != null)
+							fileOutputStream.close();
 						fromClient.close();
 						toClient.close();
 						connectionSocket.close();
@@ -66,7 +77,9 @@ public class ServerWithoutSecurity {
 				}
 
 			}
-		} catch (Exception e) {e.printStackTrace();}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
